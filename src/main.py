f39bd08206b89e458d77bb7d8315e85c27ecdf5b
Vũ Xuân Kiên 👑 <Vxkiue@gmail.com>
2025-08-01 13:48:11 +0700

    Update main.py

--- src/main.py
+++ src/main.py
@@ -5,7 +5,6 @@ from .api import example
 from . import auth
 
 app = FastAPI()
-
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
