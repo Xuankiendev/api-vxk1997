4c10fefc81c8ca748f0cb05cced8620a9508fa04
Vũ Xuân Kiên 👑 <Vxkiue@gmail.com>
2025-08-01 13:45:31 +0700

    Update db.py

--- src/db.py
+++ src/db.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine, Column, String, Integer
+from sqlalchemy import create_engine, Column, String, Integer, exc
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
@@ -12,10 +12,11 @@ class User(Base):
     __tablename__ = "users"
     
     id = Column(Integer, primary_key=True, index=True)
-    email = Column(String, unique=True, index=True)
-    password = Column(String)
-    apiKey = Column(String, unique=True)
+    email = Column(String(255), unique=True, index=True, nullable=False)
+    password = Column(String(255), nullable=False)
+    apiKey = Column(String(255), unique=True, nullable=False)
 
+Base.metadata.drop_all(bind=engine)
 Base.metadata.create_all(bind=engine)
 
 def getDb():
