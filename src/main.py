f02ff5e9ef121655b0b30874499f766163565f08
Vũ Xuân Kiên 👑 <Vxkiue@gmail.com>
2025-08-01 13:31:56 +0700

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,24 +1,25 @@
 from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-from src.api import example
-from src.db import createTables
+from .api import example
+from . import auth
 
-createTables()
 app = FastAPI()
+
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
+app.include_router(auth.router)
+app.include_router(example.router)
+
 @app.get("/")
-def home(request: Request):
+async def home(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
 @app.get("/signup")
-def signupPage(request: Request):
+async def signupPage(request: Request):
     return templates.TemplateResponse("signup.html", {"request": request})
 
 @app.get("/login")
-def loginPage(request: Request):
+async def loginPage(request: Request):
     return templates.TemplateResponse("login.html", {"request": request})
-
-app.include_router(example.router, prefix="/api")
