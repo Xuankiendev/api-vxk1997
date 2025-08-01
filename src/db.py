d865927e8c2a9af278337657e09fe217ae09bf8b
Vũ Xuân Kiên 👑 <Vxkiue@gmail.com>
2025-08-01 13:29:01 +0700

    Update db.py

--- src/db.py
+++ src/db.py
@@ -1,18 +1,26 @@
-import os, psycopg
-from psycopg.rows import dictRow
+from sqlalchemy import create_engine, Column, String, Integer
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
-def getConn():
-    return psycopg.connect(os.getenv("DATABASE_URL"), rowFactory=dictRow)
+DATABASE_URL = "postgresql://neondb_owner:npg_vdMq1IOD6wiR@ep-orange-star-a18qqv9e-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require"
 
-def createTables():
-    conn = getConn()
-    conn.execute("""
-        create table if not exists users (
-            id serial primary key,
-            email text unique,
-            password text,
-            apiKey text unique
-        )
-    """)
-    conn.commit()
-    conn.close()
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = "users"
+    
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True)
+    password = Column(String)
+    apiKey = Column(String, unique=True)
+
+Base.metadata.create_all(bind=engine)
+
+def getDb():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
