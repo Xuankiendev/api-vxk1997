b81f3cc9add23677e2988d7089eb30dec9ebe015
Vũ Xuân Kiên 👑 <Vxkiue@gmail.com>
2025-08-02 20:54:43 +0700

    Update echo.py

--- src/api/echo.py
+++ src/api/echo.py
@@ -1,11 +1,7 @@
-from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from ..auth import validateApiKey
 from ..db import getDb
 
-router = APIRouter(prefix="/api")
-
-@router.get("/echo")
-async def echo(msg: str, apiKey: str, db: Session = Depends(getDb)):
-    await validateApiKey(apiKey, db)
-    return {"message": msg}
+async def run(params: dict, db: Session):
+    await validateApiKey(params["apiKey"], db)
+    return {"message": params["msg"]}
