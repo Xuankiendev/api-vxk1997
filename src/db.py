2f6101c47c279cff55f71e644eb91efbe0da24d2
Vũ Xuân Kiên 👑 <Vxkiue@gmail.com>
2025-08-03 08:30:03 +0700

    Update db.py

--- src/db.py
+++ src/db.py
@@ -1,7 +1,6 @@
-from sqlalchemy import create_engine, Column, String, Integer, DateTime
+from sqlalchemy import create_engine, Column, String, Integer
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from datetime import datetime
 
 DATABASE_URL = "postgresql://neondb_owner:npg_vdMq1IOD6wiR@ep-orange-star-a18qqv9e-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require"
 
@@ -15,14 +14,6 @@ class User(Base):
     email = Column(String(255), unique=True, index=True, nullable=False)
     password = Column(String(255), nullable=False)
     apiKey = Column(String(255), unique=True, nullable=False)
-    createdAt = Column(DateTime, default=datetime.utcnow)
-
-class Verification(Base):
-    __tablename__ = "verifications"
-    id = Column(Integer, primary_key=True)
-    email = Column(String(255), nullable=False, unique=True)
-    code = Column(String(10), nullable=False)
-    expiresAt = Column(DateTime, nullable=False)
 
 Base.metadata.create_all(bind=engine)
 
